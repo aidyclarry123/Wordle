a87a59cc230450fbd3f05e86991d35f61ea4416a
Aidan <aidan.d.clarry@au.ey.com>
2025-06-18 11:43:21 +1000

    w

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aidyclarry123.github.io/', // Replace <REPO_NAME> with your GitHub repo name
+  base: '/', // Set base to root for user/username.github.io repo
 })
